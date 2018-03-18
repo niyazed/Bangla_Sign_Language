01a908ed6118810864f401b287aee7a40d6a59e6
Niyaz Bin Hashem <niyazed@outlook.com>
2018-03-18 22:29:21 +0600

    Dictonary added

---
+++ AndroidApp/BanglaSignLanguage/app/src/main/java/com/bananakernel/banglasignlanguage/howto.java
@@ -0,0 +1,13 @@
+package com.bananakernel.banglasignlanguage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class howto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_howto);
+    }
+}
